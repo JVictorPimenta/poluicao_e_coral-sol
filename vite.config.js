180febe8f44f7cb83d15cd67e35eff6f49330909
João Victor Pimenta R <joaovictorp.r77@gmail.com>
2025-08-08 09:29:17 -0300

    config para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/poluicao_e_coral-sol/', 
   plugins: [react()],
   resolve: {
     alias: {
